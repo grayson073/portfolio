c9d2a1c34e62629d698c84f9a6a4b8e3646812bb
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-13 12:25:02 -0700

    Adding eslintrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+const SPACE_ID = process.env.SPACE_ID;
+const DELIVERY_API_KEY = process.env.DELIVERY_API_KEY;
+// const PREVIEW_API_KEY = process.env.PREVIEW_API_KEY;
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -17,5 +33,22 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: SPACE_ID,
+        accessToken: DELIVERY_API_KEY,
+      },
+    },
+    // For PREVIEW API (includes contentful drafts)
+      // {
+      //   resolve: `gatsby-source-contentful`,
+      //   options: {
+      //     spaceId: SPACE_ID,
+      //     accessToken: PREVIEW_API_KEY,
+      //     host: `preview.contentful.com`,
+      //   },
+      // },
+    `gatsby-plugin-sass`
   ],
 }
